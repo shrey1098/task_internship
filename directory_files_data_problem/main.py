edc7ef8c59c6f37c1f09c63df969f421de8345b7
shrey1098 <shrey1098@hotmail.com>
2020-08-28 18:00:29 +0530

    init

--- directory_files_data_problem/main.py
+++ directory_files_data_problem/main.py
@@ -1,22 +1,32 @@
-
 from os import stat
 from pwd import getpwuid
-
+import csv
 import os.path, time
 
 
+# Function takes directory path and output file name (without .csv extension) as parameters
+def file_metadata(dir_path: str, output_file: str):
+    arr = os.listdir(dir_path)
+    output = output_file + ".csv"
+    with open(output, 'a') as file:
+        writer = csv.writer(file, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(['Filename', 'Filetype', 'Size(in Bytes)', 'Author', 'Last Modified',
+                         'Created', 'Last Accessed'])
+
+    for file in arr:
+        filename, file_extension = os.path.splitext(file)
+        size = os.path.getsize(dir_path + "/" + file)
+        author = getpwuid(stat(dir_path + "/" + file).st_uid).pw_name
+        modify_date = time.ctime(os.path.getmtime(dir_path + "/" + file))
+        create_date = time.ctime(os.path.getctime(dir_path + "/" + file))
+        access_date = time.ctime(os.path.getatime(dir_path + "/" + file))
+
+        with open(output, 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(
+                [filename, file_extension, size, author, modify_date, create_date, access_date])
+
+    return output
 
 
-dir = "BSHDEV-master"
-arr = os.listdir(dir)
-
-for file in arr:
-    filename, file_extension = os.path.splitext(file)
-    size = os.path.getsize( dir + "/" + file)
-    author = getpwuid(stat(dir + "/" + file).st_uid).pw_name
-    modify_date = time.ctime(os.path.getmtime(dir + "/" + file))
-    create_date = time.ctime(os.path.getctime(dir + "/" + file))
-    access_date = time.ctime(os.path.getatime(dir + "/" + file))
-
-
-    print(filename, file_extension, str(size) + "-bytes", author, modify_date, create_date, access_date)
---
+++ directory_files_data_problem/test_call.py
@@ -0,0 +1,5 @@
+from directory_files_data_problem.main import file_metadata
+
+file_metadata('BSHDEV-master', 'output')
+
+
